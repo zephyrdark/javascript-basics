880d66d612019ebfc8148dba24868a35434f87bb
zephyrdark <zephyrdark@users.noreply.github.com>
2023-11-05 16:39:23 +0800

    Complete lesson

--- lessons/fundamentals-part-2/script.js
+++ lessons/fundamentals-part-2/script.js
@@ -17,3 +17,21 @@ function webLogin() {
         alert (`I don't know you`);
     }
 }
+
+function quiz() {
+    let input = prompt(`What is the "official" name of JavaScript?`);
+    (input == `ECMAScript`) ? alert("Right!") : alert("You don't know? ECMAScript!");
+}
+
+function sign() {
+    const input = prompt("Give me a number");
+    if (input > 0) {
+        alert("1");
+    } else if (input < 0) {
+        alert("-1");
+    } else if (input == 0) {
+        alert("0");
+    } else {
+        alert("Error, please input numbers only");
+    }
+}
